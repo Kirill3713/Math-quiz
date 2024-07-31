765766c68e0c79be715d26e0042fcc7e4baff53c
Kirill <kiryap10@gmail.com>
2024-07-31 13:58:48 +0300

    Add comments, improving the appearance of the code, adjusting information, removing unnecessary variables and shortening the code.

--- check_answer.py
+++ check_answer.py
@@ -1,15 +1,18 @@
 def check_answer(primer: str, answer: int) -> bool:
-    """Функция для проверки ответа"""
+    """
+    Функция для проверки ответа
+    """
     try:
-
+        # Проверяем ответ с помощью функции eval()
         if eval(primer) == float(answer):
             return True
         else:
             return False
     except ValueError:
+        # Создаем исключение на случай неправильного ввода
         print("Введено некорректное значение.")
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     print(check_answer("2 + 3", 5))
     print(check_answer("5 * 3", 10))
     print(check_answer("10-3", "семь"))
--- generate_math_question.py
+++ generate_math_question.py
@@ -1,25 +1,23 @@
+# Импортируем модули
 import random
-import colorama
-lightwhite = colorama.Fore.LIGHTWHITE_EX
-black = colorama.Fore.BLACK
-
 
 def generate_math_question(a: int = 1, b: int = 10) -> list:
-    """Функция для генерации примера"""
+    """
+    Функция для генерации примера
+    """
+    # Создаем список операторов
     operators = ['+', '-', '*', '/']
-
+    # Выбираем случайные числа из заданного диапазона и оператор
     num1, num2 = random.randint(a, b), random.randint(a, b)
     operator = random.choice(operators)
+    # Проверяем чтобы деление было только нацело
     if operator == '/':
         num1 = num2*num1
-
-
+    # Создаем пример
     primer = f"{num1} {operator} {num2}"
+    # Возвращаем пример
     return primer
 
-
 if __name__ == "__main__":
-
     print(generate_math_question())
-
     print(generate_math_question(9, 111))
